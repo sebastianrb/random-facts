34edd6be0aa7e514a7ba5bece1d97c53efde1f26
Sebastian <sebastian.r.rb@gmail.com>
2017-11-08 15:23:28 -0500

    Bypass mixed content issue

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 $(document).ready(function($) {
     var clicked = false;
-    var $gears = $('.gears');
+    var $gears = $(".gears");
     getFact();
 
-    $('.form__submit-button').on('click', function(event) {
+    $(".form__submit-button").on("click", function(event) {
         event.preventDefault();
         clicked = true;
-        $('.random-container').addClass("form-hidden");
+        $(".random-container").addClass("form-hidden");
         //show gears
-        $gears.addClass('gears-shown');
+        $gears.addClass("gears-shown");
         getFact();
     });
 
     function getFact() {
-        var randomNumber = Math.floor((Math.random() * 100) + 1);
+        var randomNumber = Math.floor(Math.random() * 100 + 1);
         $.ajax({
-            url: 'http://numbersapi.com/' + randomNumber,
-            type: 'GET',
+            url: "https://cors-anywhere.herokuapp.com/http://numbersapi.com/" + randomNumber,
+            type: "GET",
             success: function(response) {
-                if(!clicked) {
-                    $('.random-container').removeClass("load-hidden");
-                    $('.random-container').removeClass("form-hidden");
-                    $('.random-number').text(randomNumber);
-                    $('.random-fact').text(response);
+                if (!clicked) {
+                    $(".random-container").removeClass("load-hidden");
+                    $(".random-container").removeClass("form-hidden");
+                    $(".random-number").text(randomNumber);
+                    $(".random-fact").text(response);
                 } else {
                     setTimeout(function() {
-                    $('.random-container').removeClass("form-hidden");
-                    $('.random-number').text(randomNumber);
-                    $('.random-fact').text(response);
-                    $gears.removeClass('gears-shown');
-                }, 800);
+                        $(".random-container").removeClass("form-hidden");
+                        $(".random-number").text(randomNumber);
+                        $(".random-fact").text(response);
+                        $gears.removeClass("gears-shown");
+                    }, 800);
                 }
             }
         })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-        });
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
     }
-
 });
